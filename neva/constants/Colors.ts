87d9be5ee02b371a547154224cea34768ff37521
coleguitas-team <edigalmor@outlook.com>
2024-02-29 10:38:27 -0500

    Create project

---
+++ neva/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = 'rgba(50,173,230,1)';
+const tintColorDark = 'rgba(100,210,255,1)';
+
+export default {
+  light: {
+    text: 'rgba(0,0,0,1)',
+    background: 'rgba(255,255,255,1)',
+    tint: tintColorLight,
+    tabIconDefault: 'rgba(153,153,153,1)',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: 'rgba(255,255,255,1)',
+    background: 'rgba(0,0,0,1)',
+    tint: tintColorDark,
+    tabIconDefault: 'rgba(153,153,153,1)',
+    tabIconSelected: tintColorDark,
+  },
+};
